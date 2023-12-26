1fe059813aa330e10f74c2c7ec4d1b900f366ba9
Mahrukh Aqeel <sp22bsse0025@maju.edu.pk>
2023-12-26 20:39:06 +0500

    webs

--- Project/src/main/Restaurant.java
+++ Project/src/main/Restaurant.java
@@ -13,10 +13,16 @@ public class Restaurant {
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM deliverymanagement");
             while (rs.next()) {
+            	int Delivery_ID=rs.getInt(Delivery_ID),
+            int Order_ID=rs.getInt(Order_ID),	
                 System.out.println("Delivery_ID" + rs.getInt(1) +
+<<<<<<< Updated upstream
                         " 	Name: " + rs.getString(2) +
                         " Contact " + rs.getString(3)+
                         " Status " + rs.getString(4));
+                                   " Order_ID: " + rs.getInt(1) +
+                                   " Delivery_Status " + rs.getString(3));
+>>>>>>> Stashed changes
             }
             rs.close();
             stmt.close();
