0921aa7dbb3a18e7f9def8778f970527d383934f
IMMARIUM <maryamnaz1217@gmail.com>
2023-12-28 16:02:30 +0500

    work done

---
+++ Project/src/main/Delivery.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Delivery {
+	public DeliveryDetails getDeliveryDetails(int deliveryId)
+	{
+		DeliveryDetails dd = null;
+		
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "");
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM deliverypersonnel where Delivery_ID="+deliveryId);
+            if(rs.next()) {
+            	dd = new DeliveryDetails();
+            	
+            	dd.deliverId = rs.getInt(1);
+            	dd.name = rs.getString(2);
+            	dd.contact = rs.getString(3);
+            	dd.status = rs.getString(4);
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return dd;
+	}
+}
--- Project/src/main/Invoice.java
+++ Project/src/main/Invoice.java
@@ -12,16 +12,8 @@ public class Invoice {
             ResultSet rs = stmt.executeQuery("SELECT deliverypersonnel.Contact AS PersonnelContact, menu.Name AS FoodName, menu.Price AS FoodPrice, deliverymanagement.Order_ID, deliverymanagement.Delivery_ID FROM deliverypersonnel join menu on deliverypersonnel.Delivery_ID=menu.Menu_ID");
 
             while (rs.next()) {
-                personnelContact = rs.getInt("PersonnelContact");
-                foodName = rs.getString("FoodName");
-                foodPrice = rs.getInt("FoodPrice");
-                orderID = rs.getInt("Order_ID");
-                deliveryID = rs.getInt("Delivery_ID");
-
                 System.out.println("Delivery_ID: " + deliveryID + ", Food Name: " + foodName + ", Personnel Contact: " + personnelContact + ", Food Price: " + foodPrice + ", Order_ID: " + orderID);
             }
-
-
             con.close();
         } catch (Exception e) {
             System.out.println(e);
