2926140e9cdf2913376d519de6e0c554032bfb66
IMMARIUM <maryamnaz1217@gmail.com>
2023-12-28 15:14:46 +0500

    work

--- Project/src/main/Management.java
+++ Project/src/main/Management.java
@@ -14,7 +14,9 @@ public class Management {
 
 			while(rs.next())
 			{
-			System.out.println("Delivery_ID: " + rs.getInt(1) + " Order_ID: " + rs.getInt(2) + " Delivery_Status: " + rs.getString(3) + " Estimated_Delivery_Time: " + rs.getString(4));
+				int Delivery_ID=rs.getInt(1);
+			     int Order_ID=rs.getInt(2);
+			     System.out.println("Delivery_ID: " + rs.getInt(1) + " Order_ID: " + rs.getInt(2) + " Delivery_Status: " + rs.getString(3) + " Estimated_Delivery_Time: " + rs.getString(4));
 			}
 			con.close();
 			} catch(Exception e)
