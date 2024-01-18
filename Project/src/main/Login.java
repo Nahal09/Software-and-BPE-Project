3bfa01c1c3d46c1143a3ff85c59e691c033ade25
Nahal Junaid <sp22bsse0026@maju.edu.pk>
2024-01-18 14:44:31 +0500

    error resolved

--- Project/src/main/Login.java
+++ Project/src/main/Login.java
@@ -7,22 +7,6 @@ public class Login {
 	        try {
 	            Class.forName("com.mysql.jdbc.Driver");
 	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "");
-<<<<<<< HEAD
-	            Statement stmt=con.createStatement();
-				ResultSet rs=stmt.executeQuery("select Email,password from signup where Email='"+email+"' and password='"+pswrd+"'");
-
-				while(rs.next()){
-					String Email = rs.getString(1);
-					String password = rs.getString(2);
-				}
-				con.close();
-			} catch(Exception e){
-				System.out.println(e);
-			}
-
-	        return "" ;
-=======
-	            
 	            PreparedStatement stmt = con.prepareStatement("SELECT * FROM signup WHERE Email=? AND Password=?");
 	            stmt.setString(1, email);
 	            stmt.setString(2, pswrd);
@@ -43,7 +27,6 @@ public class Login {
 	        }
 
 	        return "Failed!";
->>>>>>> nahal_webserrvice
 	    }
 
 }
