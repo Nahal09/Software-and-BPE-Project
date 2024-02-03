6cbc02dc12ee9b462cdc231a8451cbc99917d596
IMMARIUM <maryamnaz1217@gmail.com>
2024-02-03 11:23:22 +0000

    updated

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2024 at 10:04 AM
+-- Generation Time: Feb 03, 2024 at 12:09 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -26,17 +26,6 @@ USE `restaurant`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `adminlogin`
---
-
-CREATE TABLE `adminlogin` (
-  `email` varchar(50) NOT NULL,
-  `pswrd` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `adminsignup`
 --
 
@@ -55,7 +44,16 @@ CREATE TABLE `adminsignup` (
 
 INSERT INTO `adminsignup` (`First_Name`, `Last_Name`, `Email`, `Phone_Number`, `Address`, `Password`) VALUES
 ('maha', 'ahmed', 'maha12@gmail.com', 23455, 'kechs', '123'),
-('nahal', 'junaid', 'nahaljunaid03@gmail.com', 25436, '123 street', '123');
+('nahal', 'junaid', 'nahaljunaid03@gmail.com', 25436, '123 street', '123'),
+('ALIZA', 'NAZ', 'maryam133@gmail.com', 2536, 'pechs', '12345'),
+('ALIZA', 'NAZ', 'maryam133@gmail.com', 2536, 'pechs', '12345'),
+('ALIZA', 'NAZ', 'maryam133@gmail.com', 2536, 'pechs', '12345'),
+('HAYA', 'ALI', 'HAYA@GMAIL.COM', 57890, 'FYJFJ', '2002'),
+('arzoo', 'khan', 'arzoo12@gmail.com', 22344455, 'khi', '1234'),
+('haris ', 'ahmed ', 'haris12@gmail.com', 234454, 'isb', '22345'),
+('haris ', 'ahmed ', 'haris12@gmail.com', 234454, 'isb', '22345'),
+('habib', 'asif', 'habib01@gmail.com', 23545545, 'khi', '123'),
+('abc', 'nine', 'anb167@gmail.com', 2234445, 'khi', '123');
 
 -- --------------------------------------------------------
 
@@ -75,20 +73,12 @@ CREATE TABLE `deliverymanagement` (
 --
 
 INSERT INTO `deliverymanagement` (`Delivery_ID`, `Order_ID`, `Delivery_Status`, `Estimated_Delivery_Time`) VALUES
-(1, 1, 'Pending', '2023-12-23 18:44:03');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `deliverypersonnel`
---
-
-CREATE TABLE `deliverypersonnel` (
-  `Delivery_ID` int(11) NOT NULL,
-  `Name` varchar(50) DEFAULT NULL,
-  `Contact` varchar(20) DEFAULT NULL,
-  `Status` enum('Available','Busy') DEFAULT 'Available'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(1, 1, 'Pending', '2023-12-23 18:44:03'),
+(22, 22, 'Delivered', '2024-01-18 15:54:40'),
+(8, 8, 'Pending', '2024-01-18 16:05:19'),
+(23, 335, 'Delivered', '2024-01-21 08:22:06'),
+(15, 15, 'Pending', '2024-02-03 08:56:31'),
+(50, 50, 'Delivered', '2024-02-03 08:57:08');
 
 -- --------------------------------------------------------
 
@@ -109,18 +99,12 @@ CREATE TABLE `foodpreparation` (
 INSERT INTO `foodpreparation` (`Prep_ID`, `Order_ID`, `Preparation_Status`) VALUES
 (1, 1, 'Pending'),
 (2, 2, ''),
-(3, 3, 'In Progress');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE `login` (
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(3, 3, 'In Progress'),
+(7, 7, 'In Progress'),
+(17, 17, 'Pending'),
+(20, 20, 'Completed'),
+(21, 21, 'Completed'),
+(256, 335, 'Completed');
 
 -- --------------------------------------------------------
 
@@ -141,7 +125,7 @@ CREATE TABLE `menu` (
 
 INSERT INTO `menu` (`Menu_ID`, `Name`, `Price`, `Category`) VALUES
 (2, 'beef burger ', '350.00', 'fast food  '),
-(3, 'large pizza', '2000.00', 'fast food'),
+(7, 'Chicken Zinger ', '500.00', 'fast food '),
 (10, 'lasagna', '1100.00', 'fast');
 
 -- --------------------------------------------------------
@@ -170,7 +154,10 @@ INSERT INTO `signup` (`First Name`, `Last Name`, `Email`, `Phone Number`, `Addre
 ('maha ', 'ali', 'maha12@gmail.com', 283902, 'kechs ', '123'),
 ('maha', 'ali', 'maha12@gmail.com', 235364, 'kechs', '123'),
 ('muhammad', 'ahmed', 'ahmed12@gmail.com', 23455, 'Tariq Bin Ziyad', '123'),
-('arooba ', 'junaid', 'arooba12@gmail.com', 234556, 'malir ', '124');
+('arooba ', 'junaid', 'arooba12@gmail.com', 234556, 'malir ', '124'),
+('maryam', 'naz', 'maryam13@gmail.com', 2536, 'pechs', '12345'),
+('maryam', 'NAZ', 'maryam100@gmail.com', 2536, '', ''),
+('ali', 'ahmed', 'ali@gmail.com', 2355, 'khi', '123');
 
 --
 -- Indexes for dumped tables
@@ -182,12 +169,6 @@ INSERT INTO `signup` (`First Name`, `Last Name`, `Email`, `Phone Number`, `Addre
 ALTER TABLE `deliverymanagement`
   ADD KEY `Delivery_ID` (`Delivery_ID`),
   ADD KEY `Order_ID` (`Order_ID`);
-
---
--- Indexes for table `deliverypersonnel`
---
-ALTER TABLE `deliverypersonnel`
-  ADD PRIMARY KEY (`Delivery_ID`);
 
 --
 -- Indexes for table `foodpreparation`
@@ -208,22 +189,16 @@ ALTER TABLE `menu`
 --
 
 --
--- AUTO_INCREMENT for table `deliverypersonnel`
---
-ALTER TABLE `deliverypersonnel`
-  MODIFY `Delivery_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `foodpreparation`
 --
 ALTER TABLE `foodpreparation`
-  MODIFY `Prep_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Prep_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=257;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `Menu_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Menu_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
